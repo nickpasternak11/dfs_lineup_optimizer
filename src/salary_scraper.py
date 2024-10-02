8a3c57147500ee789b58924500887a76b169ec54
nickpasternak11 <nick.pasternak11@gmail.com>
2024-10-02 03:43:51 +0000

    updated salary scraper to use remote webdriver

--- src/salary_scraper.py
+++ src/salary_scraper.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from time import sleep
+from io import StringIO
 
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -8,18 +9,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from utils import get_current_week
+import requests
 
+
+url = "https://draftdime.com/nfl-projections"
+current_year = datetime.now().year
+current_week = get_current_week(year=current_year)
+
+def check_webdriver_container():
+    # wait until webdriver container is up
+    print("Waiting for selenium webdriver..")
+    while True:
+        try:
+            requests.get('http://selenium-web-driver:4444/wd/hub')
+            break
+        except:
+            continue
+    print("Webdriver is up!\n")
 
 def main():
-    url = "https://draftdime.com/nfl-projections"
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
-    current_year = datetime.now().year
-    current_week = get_current_week(year=current_year)
-
-    op = webdriver.ChromeOptions()
-    op.add_argument("headless")
-
-    with webdriver.Chrome(options=op) as browser:
+    print("Scraping salary data..")
+    with webdriver.Remote(command_executor='http://selenium-web-driver:4444/wd/hub', options=options) as browser:
         browser.get(url)
         sleep(1)
         site_select = Select(browser.find_element(by=By.ID, value="site-id"))
@@ -38,12 +53,13 @@ def main():
         soup = BeautifulSoup(html, "html.parser")
         table = soup.find("table", id="prj-main-table")
 
-    salary_df = pd.io.html.read_html(str(table))[0].iloc[1:, :6]
+    salary_df = pd.io.html.read_html(StringIO(str(table)))[0].iloc[1:, :6]
     salary_df.columns = ["player", "extra", "position", "team", "opponent", "salary"]
     salary_df = salary_df.drop(columns=["extra"])
     salary_df["opponent"] = salary_df.opponent.str.replace("@", "")
     salary_df["salary"] = salary_df.salary.str.replace("$", "").str.replace(",", "").astype(int)
 
+    print("Saving salary data..")
     output_path = os.path.join(
         os.path.dirname(__file__), f"../data/salaries/dk_salary_{current_year}_w{current_week}.csv"
     )
@@ -51,4 +67,5 @@ def main():
 
 
 if __name__ == "__main__":
+    check_webdriver_container()
     main()
